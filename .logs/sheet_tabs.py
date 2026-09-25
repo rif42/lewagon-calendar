ba8bc936d82b1de527af97f5afba4edcde4f53f6
rif42-pc <rifkyariya42@gmail.com>
2026-09-25 22:36:58 +0800

    security: purge leaked Google API key and scratch dirs from tracking; add gitignore

--- .logs/sheet_tabs.py
+++
@@ -1,8 +0,0 @@
-import re
-html = open('.logs/sheet-html.html', encoding='utf-8').read()
-print('len', len(html))
-for pat in [r'sheets?[^<]{0,80}', r'gid[^0-9]{0,5}\d+', r'Tally[^<]{0,120}', r'youtube[^<]{0,120}', r'BkO7h2oF6p4']:
-    hits = re.findall(pat, html, re.I)[:8]
-    print('PAT', pat, '->', len(hits))
-    for h in hits[:5]:
-        print('   ', h[:160])
--- .task/probe_detail3.py
+++
@@ -1,9 +0,0 @@
-import re, itertools, io, sys
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8", errors="replace")
-html = open(r"C:\code\lewagon-calendar\.task\probe_detail.html", encoding="utf-8").read()
-i = html.find("21 October 2026")
-print(re.sub(r"\s+", " ", html[max(0,i-2500):i+2500]))
-print("="*100)
-lst = open(r"C:\code\lewagon-calendar\.task\probe_October.html", encoding="utf-8").read()
-j = lst.find("upcoming-events/")
-print(re.sub(r"\s+", " ", lst[max(0,j-1500):j+3000]))
